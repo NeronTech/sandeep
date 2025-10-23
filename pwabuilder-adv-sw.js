a29628c458a0388fcf62ca835bafe2f7d445f0f4
NeronTech <nerontech.dev@gmail.com>
2025-10-23 16:52:16 +0800

    remove unecessary files

--- pwabuilder-adv-sw.js
+++
@@ -1,3 +0,0 @@
-
-  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
-  precacheAndRoute([{"revision":"12227c698c00b34bc0fc2522b21d8552","url":"app.js"},{"revision":"06f7a4d16bc62828d2db9532f32b8c83","url":"index.html"},{"revision":"79b488e8784c1260c00a1434496c2f5c","url":"offline.html"},{"revision":"c18cae0c3f9acf5f012b5f0f00fdd6c8","url":"service-worker.js"},{"revision":"3296abad528c5d37c176efda8b9f0cca","url":"sw.js"}]);
